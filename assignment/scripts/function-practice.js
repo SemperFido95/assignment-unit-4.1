9d9556b5a1c915568ef1b27fb636c6a5ed985651
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-01-27 16:01:32 -0500

    completed questions 9 & 10

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -88,23 +88,47 @@ console.log('Test - should return false', find('grape', testArray1));
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
-function isFirstLetter(letter, string) {
 
-}
+let isFirstLetter = (letter, string) => letter === string[0] ? true : false;
+//arrow function and ternary operator to make code as concise as possible :)
+
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+
+let numArray = [1, 2, 7, 4];
+
+function sumAll(array) {
   let sum = 0
   // TODO: loop to add items
+  for (let i of array) {
+    sum += i;
+  }
   return sum;
 }
+
+console.log('Test - should return the number 14:', sumAll(numArray));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
+let allNums = [1, -8, 0, -10, 54];
+let noPositives = [0, -1, -4];
+
+function getPositiveNums (array) {
+  let positiveNums = [];
+  for (let i of array) {
+    if (i > 0) {
+      positiveNums.push(i);
+    }
+  }
+  return positiveNums;
+}
+
+console.log('Test - Should return array of positive numbers', getPositiveNums(allNums));
+console.log('Test - should return an empty array', getPositiveNums(noPositives));
 
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
